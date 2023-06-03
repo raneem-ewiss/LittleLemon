8f8d57eb7afe46d7e000a0fe7d99d1a659f1a1d4
raneem-ewiss <132105794+raneem-ewiss@users.noreply.github.com>
2023-06-03 09:54:07 +0400

    Update settings.py

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'LittleLemonAPI.apps.LittlelemonapiConfig',
+    'restaurant.apps.restaurantConfig',
     'rest_framework.authtoken',
     'djoser',
 ]
